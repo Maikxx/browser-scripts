308546b3a2b06bfaa637343679ee1e4f84899647
Maikel van Veen <maikel.van.veen@outlook.com>
2022-12-19 09:02:10 +0100

    Add toggling between multiple languages (and all available)

--- src/scripts/cardmarket-add-language-buttons.js
+++ src/scripts/cardmarket-add-language-buttons.js
@@ -12,101 +12,172 @@
 (function() {
     'use strict';
 
-    renderButton(
-        '🇬🇧',
-        () => navigateTo(1),
-        'Show English cards only',
-        6
-    );
+    const AVAILABLE_LANGUAGES_INPUT_ELEMENTS_SELECTOR = '#articleFilterProductLanguage input';
+    const LANUAGE_URL_PARAM_KEY = 'language';
 
-    renderButton(
-        '🇫🇷',
-        () => navigateTo(2),
-        'Show French cards only',
-        5
-    );
+    const availableLanguagesInputElements = document.querySelectorAll(AVAILABLE_LANGUAGES_INPUT_ELEMENTS_SELECTOR);
+    
+    const searchParams = new URLSearchParams(window.location.search);
+    const languageSearchParam = searchParams.get(LANUAGE_URL_PARAM_KEY);
+    const selectedLanguages = getSelectedLanguageIds(languageSearchParam);
 
-    renderButton(
-        '🇩🇪',
-        () => navigateTo(3),
-        'Show German cards only',
-        4
-    );
+    renderLanguageButtons();
 
-    renderButton(
-        '🇪🇸',
-        () => navigateTo(4),
-        'Show Spanish cards only',
-        3
-    );
+    function renderLanguageButtons() {
+        [...availableLanguagesInputElements].reverse().forEach((inputElement, index) => {
+            const languageValue = inputElement.getAttribute('value');
 
-        renderButton(
-        '🇮🇹',
-        () => navigateTo(5),
-        'Show Italian cards only',
-        2
-    );
+            if (!languageValue) {
+                console.error(`Failed to get language id for input element with index: ${index}`);
+                return;
+            }
 
-    renderButton(
-        '🇵🇹',
-        () => navigateTo(8),
-        'Show Portuguese cards only',
-        1
-    );
+            const labelElement = inputElement.nextElementSibling;
 
-    renderButton(
-        '🇳🇱',
-        () => navigateTo(12),
-        'Show Dutch cards only',
-        0
-    );
+            if (!labelElement) {
+                console.error(`Failed to find a label for input element with index: ${index}`);
+                return;
+            }
 
-    function renderButton(languageFlag, callback, title, index) {
+            const iconElement = labelElement.querySelector('.icon');
+            const textElement = labelElement.querySelector('span:nth-child(2)');
+            
+
+            if (!iconElement) {
+                console.error(`Failed to find an icon for input element with index: ${index}`);
+                return;
+            }
+
+            if (!textElement) {
+                console.error(`Failed to find a text element for input element with index: ${index}`);
+                return;
+            }
+
+            const text = textElement.innerText;
+
+            if (!text) {
+                console.error(`Failed to find a language text for input element with index: ${index}`);
+                return;
+            }
+
+            renderButton(
+                iconElement.cloneNode(true),
+                languageId => navigateTo(languageId),
+                `Show ${text} cards`,
+                index,
+                Number(languageValue)
+            )
+        });
+    }
+
+    /**
+     * @param {HTMLElement} iconElement 
+     * @param {Function} callback 
+     * @param {string} title 
+     * @param {number} index 
+     * @param {number} languageId 
+     */
+    function renderButton(iconElement, callback, title, index, languageId) {
         const buttonElement = document.createElement('button');
 
-        setButtonStyling(buttonElement, index)
-        buttonElement.innerText = languageFlag;
-        buttonElement.addEventListener('click', callback);
+        setButtonStyling(buttonElement, index, languageId)
+
+        // Remove unnecessary attributes from iconElement
+        removeAttributesFromIconElement(iconElement);
+
+        buttonElement.appendChild(iconElement);
+        buttonElement.addEventListener('click', () => callback(languageId));
         buttonElement.setAttribute('title', title);
 
         document.body.appendChild(buttonElement);
     }
 
-    function setButtonStyling(element, index) {
-        element.style.position = 'fixed';
-        element.style.bottom = `${(index + 1) * 16 + index * 36}px`;
-        element.style.left = '16px';
-        element.style.backgroundColor = '#ffffff';
-        element.style.border = '0';
-        element.style.borderRadius = '8px';
-        element.style.padding = '0 8px';
-        element.style.cursor = 'pointer';
-        element.style.boxShadow = '5px 5px 5px 0px rgba(0,0,0,0.25)';
-        element.style.transition = '300ms ease all';
-        element.style.fontSize = '26px';
+    /**
+     * @param {HTMLElement} iconElement 
+     */
+    function removeAttributesFromIconElement(iconElement) {
+        const attributesToRemove = [
+            'class',
+            'data-toggle',
+            'data-html',
+            'data-placement',
+            'onmouseover',
+            'onmouseout',
+            'data-original-title',
+            'title'
+        ];
 
-        element.addEventListener('mouseenter', () => {
-            element.style.transform = 'translateY(-4px)';
-        });
-
-        element.addEventListener('mouseleave', () => {
-            element.style.transform = 'translateY(0)';
+        attributesToRemove.forEach(attributeName => {
+            iconElement.removeAttribute(attributeName);
         });
     }
 
     /**
-     * @param {number} languageNumber
+     * @param {HTMLElement} element
+     * @param {number} index
+     * @param {number} languageId
      */
-    function navigateTo(languageNumber) {
-        const searchParams = new URLSearchParams(window.location.search);
-        const languageSearchParam = searchParams.get('language');
+    function setButtonStyling(element, index, languageId) {
+        const SPACING = 16;
+        const isSelected = selectedLanguages.includes(languageId);
+        
+        element.style.display = 'flex';
+        element.style.alignItems = 'center';
+        element.style.position = 'fixed';
+        element.style.left = '8px';
+        element.style.border = isSelected ? '2px solid rgb(40,167,69)' : '0';
+        element.style.backgroundColor = '#ffffff';
+        element.style.borderRadius = '8px';
+        element.style.padding = '16px';
+        element.style.cursor = 'pointer';
+        element.style.boxShadow = '5px 5px 5px 0px rgba(0,0,0,0.25)';
+        
+        setTimeout(() => {
+            element.style.bottom = `${(index + 1) * SPACING + index * element.offsetHeight}px`;
+        }, 0)
+    }
 
-        searchParams.set(
-            'language', 
-            languageSearchParam
-                ? `${languageSearchParam}${languageSearchParam.includes(languageNumber) ? '' : `,${languageNumber}`}`
-                : languageNumber
-        );
+    /**
+     * @param {string | null} languageSearchParam
+     * @returns {number[]}
+     */
+    function getSelectedLanguageIds(languageSearchParam) {
+        if (!languageSearchParam) return [];
+
+        const selectedLanguages = languageSearchParam.split(',');
+
+        return selectedLanguages.map(Number);
+    }
+
+    /**
+     * @param {number} languageId
+     */
+    function navigateTo(languageId) {
+        // Set the first language to the url
+        if (!selectedLanguages || !selectedLanguages.length) {
+            searchParams.set(
+                LANUAGE_URL_PARAM_KEY, 
+                String(languageId)
+            );
+        } else {
+            const languageAlreadyChosen = selectedLanguages.includes(languageId);
+
+            // Remove the language from the list of languages
+            if (languageAlreadyChosen) {
+                searchParams.set(
+                    LANUAGE_URL_PARAM_KEY, 
+                    selectedLanguages.reduce((out, selectedLanguage) => 
+                        selectedLanguage !== languageId ? `${out},${selectedLanguage}` : out
+                    , '')
+                );
+            } else {
+                // Add the language to the list of languages
+                searchParams.set(
+                    LANUAGE_URL_PARAM_KEY, 
+                    selectedLanguages.concat([languageId]).join(',')
+                );
+            }
+        }
 
         window.location.search = searchParams.toString();
     }
